242a3bae748dbc4d5fd0c048ab2d88e825b70b96
compasss <crazymingzai@gmail.com>
2020-08-31 00:10:57 +0800

    remove extra files

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,25 @@
 //index.js
 Page({
   data: {
-    
+    page: 1,
+    pageSize: 10,
+    dataList: []
+  },
+  onLoad() {
+    this.getDataList()
+  },
+  getDataList() {
+    wx.cloud.callFunction({
+      name: 'goodsList',
+      data: {
+        page: this.data.page,
+        pageSize: this.data.pageSize
+      }
+    }).then(res => {
+      this.setData({
+        dataList: res.result.data
+      })
+    })
   }
+  
 })
